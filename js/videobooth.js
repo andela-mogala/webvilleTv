21edbc044c75d4d63840f9cfaba3f4a5ba826a44
Michael Ogala <michael.ogala@andela.com>
2017-01-31 21:21:36 +0100

    add western, scifi and cartoon effects

--- js/videobooth.js
+++ js/videobooth.js
@@ -178,3 +178,31 @@ function noir(pos, r, g, b, data) {
     data[pos * 4 + 2] = brightness
 }
 
+function western(pos, r, g, b, data) {
+  let brightness = (3 * r + 4 * g + b) >>> 3
+  data[pos * 4 + 0] = brightness + 40
+  data[pos * 4 + 1] = brightness + 20
+  data[pos * 4 + 2] = brightness - 20
+}
+
+function scifi(pos, r, g, b, data) {
+  let offset = pos * 4
+  data[offset] = Math.round(255 - r)
+  data[offset + 1] = Math.round(255 - g)
+  data[offset + 2] = Math.round(255 - b)
+}
+
+function bwcartoon(pos, r, g, b, outputData) {
+  let offset = pos * 4
+  if (outputData[offset] < 120) {
+    outputData[++offset] = 80
+    outputData[++offset] = 80
+    outputData[++offset] = 80
+  } else {
+    outputData[++offset] = 255
+    outputData[++offset] = 255
+    outputData[++offset] = 255
+  }
+  outputData[++offset] = 255
+  ++offset
+}
